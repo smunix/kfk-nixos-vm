f1e2146d78eae4ce3ea87d505acf5b90916f7204
Providence Salumu <Providence.Salumu@smunix.com>
2021-01-28 14:39:45 -0500

    initial commit

--- flake.nix
+++ flake.nix
@@ -2,11 +2,10 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
 
   outputs = { self, nixpkgs }: {
-
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules =
-        [ ({ pkgs, ... }:
+    packages.x86_64-linux = {
+      vm = (import "${nixpkgs}/nixos" {
+        system = "x86_64-linux";
+        configuration = { config, pkgs, ...}:
           let username = "authenticator";
           in {
             networking.firewall.allowedTCPPorts = [ 3000 ];
@@ -18,25 +17,8 @@
                 "${username}".isSystemUser = true;
               };
             };
-            
-            # boot.isContainer = true;
-
-            # # Let 'nixos-version --json' know about the Git revision
-            # # of this flake.
-            # system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-            # # Network configuration.
-            # networking.useDHCP = false;
-            # networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # # Enable a web server.
-            # services.httpd = {
-            #   enable = true;
-            #   adminAddr = "morty@example.org";
-            # };
-          })
-        ];
+          };
+      }).vm;
     };
-
   };
 }
